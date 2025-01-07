6a153ed53d8bb551566deaf0f9bbb1514e922693
Siddhi Anil Bhujbal <161146376+siddhibhujbal@users.noreply.github.com>
2025-01-07 18:59:14 +0000

    datatype_summary

---
+++ 01_basics/06_datatype_summary.js
@@ -0,0 +1,18 @@
+//JS is dynamically typed language
+
+
+//primtive
+//7 types:String,Number,Boolean,null,undefined,symbol,BigInt
+const id=Symbol('123')
+console.log(id);
+
+
+
+
+//reference(non primitive)
+//array,objects,function
+const heros=["a","b","c"]
+let myObj={
+    name:"siddhi",
+    age:"20"
+}
