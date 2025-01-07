33f09dce2b5109f409cddf1beee9a4d0e00e5707
Siddhi Anil Bhujbal <161146376+siddhibhujbal@users.noreply.github.com>
2025-01-07 18:10:34 +0000

    added new file for datatype conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,33 @@
+let score=33
+console.log(typeof score);
+
+let mark="90"
+console.log(typeof mark);
+
+//conversion to number
+let valueInNum=Number(mark)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+let num="2abc"
+let numinnum=Number(num);
+console.log();
+console.log(numinnum);
+
+//conversion
+//"33" => 33
+//"33abs" =>NaN
+//true => 1
+//false => 0
+
+//conversion in boolean
+let isLoggedIn="siddhi"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0=>false ; ""=>false; "siddhi"=>true
+
+//conversion to string
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+
