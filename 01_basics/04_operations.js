41ef9eeb5a06cb9cbb1096f4fa1ff04f5b3c884c
Siddhi Anil Bhujbal <161146376+siddhibhujbal@users.noreply.github.com>
2025-01-07 18:26:22 +0000

    added new file for datatype conversion operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,10 @@
+let value=3
+let negValue=-value
+console.log(negValue);
+
+//string to num confusing conversion
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+//more confusing conversion
+console.log(+true);
