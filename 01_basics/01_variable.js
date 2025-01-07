ee310183303dcc7dc60501e3598d4c4515225582
Siddhi Anil Bhujbal <161146376+siddhibhujbal@users.noreply.github.com>
2025-01-07 16:59:18 +0000

    added new file for learning variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=1434
+let accountEmail="abc@gmail.com"
+
+//do not use var
+var accountPassword="1234"
+accountCity="Pune"
+
+// accountId=1 //const var cant be changed
+console.log(accountId);
