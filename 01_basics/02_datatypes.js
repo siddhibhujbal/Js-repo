48c400b72ae824b7bd0aa00be15e63a0c0356a5b
Siddhi Anil Bhujbal <161146376+siddhibhujbal@users.noreply.github.com>
2025-01-07 17:43:41 +0000

    added new file for datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";//the engine treats js code as newer version
+
+// alert("hello") //this can be done in browser and not in node js there is other syntax to be seen later
+
+console.log(3+
+    5
+) //this works but  u should take care of code readability
