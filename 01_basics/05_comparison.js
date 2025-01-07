0b66af2c61cd116a48aa8b872c2b8f1499b9edcc
Siddhi Anil Bhujbal <161146376+siddhibhujbal@users.noreply.github.com>
2025-01-07 18:37:09 +0000

    added new file for comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,16 @@
+console.log(null > 0);
+console.log(null==0);//this works different than comparison
+console.log(null>=0);//this gives true bcause in js comparison operator converts null to number
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+
+
+//strict check => not only check value but also check datatypes
+console.log("2"===2);
+
+
